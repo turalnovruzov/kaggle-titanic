9965af575d4f95f4bb41d7d8222f4487da91840a
Tural Novruzov <tural.nt.1@gmail.com>
2020-08-20 13:21:37 +0400

    Added utils.py. Added param_curve

---
+++ utils.py
@@ -0,0 +1,19 @@
+def param_curve(model, param, start=1, end=100, step=1, verbose=True):
+    x_ax = []
+    scores = []
+    val_scores = []
+
+    for n in np.arange(start, end, step):
+        model.set_params(**{param: n})
+        model.fit(X, y)
+        x_ax.append(n)
+        scores.append(model.score(X, y))
+        val_scores.append(cross_val_score(model, X, y).mean())
+        if verbose:
+            print(f'n = {n}\tscore = {scores[-1]}\tval score = {val_scores[-1]}')
+    
+    plt.plot(x_ax, scores, label='Train scores')
+    plt.plot(x_ax, val_scores, label='Validation scores')
+    plt.xlabel(param)
+    plt.ylabel('Cost')
+    plt.show()
